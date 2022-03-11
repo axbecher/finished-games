7dd1106f59c08c53d4936a102a9875b4a6f2a5af
Alexandru Becher <becheralexandru@gmail.com>
2022-03-11 14:59:14 +0200

    -

--- script.js
+++ script.js
@@ -8,19 +8,51 @@ http.onload = function(){
     if(this.readyState == 4 && this.status == 200){
         let informatii = JSON.parse(this.responseText);
         let output = "";
+        let statistici = "";
+        let countFinished = 0;
+        let countAllGames = 0;
 
         for(let item of informatii){
+            if(parseInt(item.Progress)==parseInt(100)){
+                countFinished = countFinished + 1;
+            }
+            if(item.Status){
+                countAllGames = countAllGames + 1;
+            }
+            
+            
             output += `
                 <div class="game">
-                <img src="${item.GameImage}" alt="${item.GameTitle}"></img>
-                <p class="title">${item.GameTitle}</p>
-                <p class="progress">Progress: ${item.Progress}</p>
-                <p class="platform">Platform: ${item.Platform}</p>
-                <p class="hoursPlayed">Hours Played: ${item.Total_Hours_Played}</p>
-                <p class="tags">Tags: ${item.Tags}</p>
+                <a title="${item.GameTitle}" href="${item.AccesLink}" target="_blank"><img src="${item.GameImage}" alt="${item.AccesLink}"></img></a>
+                <p class="progress">${item.Progress}%</p>
+                <p class="gameTitle">${item.GameTitle}</p>
+                <div class="hoursPlayed">${item.Total_Hours_Played} hours</div>
+                <p class="platformImg"> <img  src="platforms/${item.Platform}.png" alt="${item.Platform}"></img> </p>
                 </div>
                 `;
         }
+        statistici += `<p> Finished Games Number in Total: [ ${countFinished} / ${countAllGames} ]  </p>`
         document.querySelector(".games").innerHTML = output;
+        document.querySelector(".analytics").innerHTML = statistici;
     }
+}
+
+
+var mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
