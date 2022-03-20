8dd68a789089879ab7c4b427b99f72ed09f9d1c2
Alexandru Becher <becheralexandru@gmail.com>
2022-03-20 14:48:11 +0200

    added loading screen

--- script.js
+++ script.js
@@ -21,14 +21,27 @@ http.onload = function(){
             }
             
             
+            
             output += `
-                <div class="game">
-                <a title="${item.GameTitle}" href="${item.AccesLink}" target="_blank"><img src="${item.GameImage}" alt="${item.AccesLink}"></img></a>
-                <p class="progress">${item.Progress}%</p>
-                <p class="gameTitle">${item.GameTitle}</p>
-                <div class="hoursPlayed">${item.Total_Hours_Played} hours</div>
-                <p class="platformImg"> <img  src="platforms/${item.Platform}.png" alt="${item.Platform}"></img> </p>
-                </div>
+            <div class="padding-item">
+              <div class="card-item">
+                  <div class="gameImage">
+                    <a title="${item.GameTitle}" href="${item.AccesLink}" target="_blank"><img src="${item.GameImage}" alt="${item.AccesLink}"></img></a>
+                  </div>
+                  <div class="gameTitle-area">
+                    <p class="gameTitle">${item.GameTitle}</p>
+                  </div>
+                  <div class="progress-area">
+                    <p class="progressArea">${item.Progress}%</p>
+                  </div>
+                  <div class="hoursPlayed-area">
+                    <div class="hoursPlayed">${item.Total_Hours_Played} hours</div>
+                  </div>
+                  <div class="platform-area">
+                    <p class="platformImg"> <img  src="platforms/${item.Platform}.png" alt="${item.Platform}"></img> </p>
+                  </div>
+              </div>
+            </div>
                 `;
         }
         statistici += `<p> Finished Games Number in Total: [ ${countFinished} / ${countAllGames} ]  </p>`
@@ -55,4 +68,48 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+
+//$(window).load(function() {  $("#tabs").removeClass("stillLoading"); });
+
+document.onreadystatechange = function(e)
+{
+  if(document.readyState=="interactive")
+  {
+    var all = document.getElementsByTagName("*");
+    for (var i=0, max=all.length; i < max; i++) 
+    {
+      set_ele(all[i]);
+    }
+  }
+}
+
+function check_element(ele)
+{
+  var all = document.getElementsByTagName("*");
+  var totalele=all.length;
+  var per_inc=100/all.length;
+
+  if($(ele).on())
+  {
+    var prog_width=per_inc+Number(document.getElementById("progress_width").value);
+    document.getElementById("progress_width").value=prog_width;
+    $("#bar1").animate({width:prog_width+"%"},10,function(){
+      if(document.getElementById("bar1").style.width=="100%")
+      {
+        $(".progress").fadeOut("slow");
+      }			
+    });
+  }
+
+  else	
+  {
+    set_ele(ele);
+  }
+}
+
+function set_ele(set_element)
+{
+  check_element(set_element);
 }
